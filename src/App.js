53666fad2e93861c11cf2222052cfa112157ace4
thealekid <chukorakwusi@gmail.com>
2020-10-03 20:01:28 +0100

    Form

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ const App = () => {
   const APP_ID = "ee0bfc54";
   const APP_KEY = "99333c79b9c5c2af50a8f368137615ec"
 
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
-    <div className="App">
-    <h1>Recipe</h1>
+    <div className="App"> 
+    <form className="search-form">
+      <input className="search-bar" type="text"/>
+      <button className="search-button" type="submit">
+      Search
+      </button>
+    </form>
     </div>
   )
 }
