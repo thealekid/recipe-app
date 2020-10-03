9c3fa8b4882d19dc1509cdfacaa5c6b8b63b2a37
thealekid <chukorakwusi@gmail.com>
2020-10-03 20:13:02 +0100

    Counter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = "ee0bfc54";
   const APP_KEY = "99333c79b9c5c2af50a8f368137615ec"
-
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect (() => {
+    console.log('UseEffect');
+  })
 
   return (
     <div className="App"> 
     <form className="search-form">
       <input className="search-bar" type="text"/>
-      <button className="search-button" type="submit">
-      Search
+      <button onClick={() => setCounter(counter + 1)} 
+      className="search-button" 
+      type="submit"
+      >
+      {counter}
       </button>
     </form>
     </div>
