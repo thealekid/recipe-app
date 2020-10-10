53a8ede7a6bdd36e359f82a16d19c81e7329a559
thealekid <chukorakwusi@gmail.com>
2020-10-10 18:55:17 +0100

    counterUpdate

--- src/App.js
+++ src/App.js
@@ -17,15 +17,13 @@ const App = () => {
     <div className="App"> 
     <form className="search-form">
       <input className="search-bar" type="text"/>
-      <button onClick={() => setCounter(counter + 1)} 
-      className="search-button" 
-      type="submit"
-      >
-      {counter}
+      <button className="search-button" type="submit">
+        Search
       </button>
-      
     </form>
+    <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
+
   )
 }
 
